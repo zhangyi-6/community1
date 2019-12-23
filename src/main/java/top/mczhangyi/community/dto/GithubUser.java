93831f10f6a684e4ee0c511625e1657f66a8560c
zhangyi <3414617086@qq.com>
2019-12-23 20:08:33 +0800

    extract github config from code to properties

---
+++ src/main/java/top/mczhangyi/community/dto/GithubUser.java
@@ -0,0 +1,35 @@
+package top.mczhangyi.community.dto;
+
+/**
+ * 张易
+ * 2019/12/23-12:04
+ */
+public class GithubUser {
+    private String name;
+    private Long id;
+    private String bio;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
